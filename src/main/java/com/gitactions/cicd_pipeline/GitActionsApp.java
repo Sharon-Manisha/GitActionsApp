5ea8284cccfad755018300bb93791c7c9cf708c4
Sharon-Manisha <72428583+Sharon-Manisha@users.noreply.github.com>
2023-02-15 21:15:43 +0530

    message change

--- src/main/java/com/gitactions/cicd_pipeline/GitActionsApp.java
+++ src/main/java/com/gitactions/cicd_pipeline/GitActionsApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitActionsApp {
 	public static void main(String[] args)  
 	{  
-		System.out.println( "Hello App!" );
+		System.out.println( "Hello App - Sharon!" );
 	SpringApplication.run(GitActionsApp.class, args);    
 	}   
 }
